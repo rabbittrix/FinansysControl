c76e4e13621040eec5a19dc27b82ba5125840392
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-17 00:30:22 +0100

    create router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
+  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
+  { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' },
+
+  { path: '', redirectTo: '/reports', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

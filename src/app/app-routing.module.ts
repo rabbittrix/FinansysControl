f8a2b2dcc6c10f0d2daffe5ce46e36c1ce894961
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-20 01:09:34 +0100

    create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
-  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
-  { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' },
+const routes: Routes = [ // { path: 'auth', loadChildren: () => import('src/app/auth/auth.module').then(m => m.AuthModule) }
+  { path: 'entries', loadChildren: () => import('src/app/pages/entries/entries.module').then(m => m.EntriesModule)},
+  { path: 'categories', loadChildren: () => import('src/app/pages/categories/categories.module').then(m => m.CategoriesModule) },
+  { path: 'reports', loadChildren: () => import('src/app/pages/reports/reports.module').then(m => m.ReportsModule) },
 
   { path: '', redirectTo: '/reports', pathMatch: 'full' }
-];
+]; //'./pages/entries/entries.module#EntriesModule' 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

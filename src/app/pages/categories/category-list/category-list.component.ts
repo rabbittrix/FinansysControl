1c900188fab1d96b35ac1fe3e3cf3d029b0822a6
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-20 01:13:17 +0100

    new component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { BaseResourceListComponent } from "../../../shared/components/base-resource-list/base-resource-list.component";
+
+import { Category } from "../shared/category.model";
+import { CategoryService } from "../shared/category.service";
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private categoryService: CategoryService) { 
+    super(categoryService);
   }
 
 }

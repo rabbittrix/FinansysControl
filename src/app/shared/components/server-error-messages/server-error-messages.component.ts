746bd4f83eed9343c4108cbf6dc0b99141d31d73
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-20 01:17:37 +0100

    new component

---
+++ src/app/shared/components/server-error-messages/server-error-messages.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-error-messages',
+  templateUrl: './server-error-messages.component.html',
+  styleUrls: ['./server-error-messages.component.css']
+})
+export class ServerErrorMessagesComponent implements OnInit {
+
+  // tslint:disable-next-line:no-input-rename
+  @Input('server-error-messages') serverErrorMessages: string[] = null;
+
+  constructor() { }
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+  }
+
+}

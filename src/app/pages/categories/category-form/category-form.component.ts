6e499afa3f6837c3e9f531eb343a824e24f803c5
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-20 01:12:58 +0100

    new component

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { Validators } from '@angular/forms';
+
+import { BaseResourceFormComponent } from '../../../shared/components/base-resource-form/base-resource-form.component';
+
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
+
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent extends BaseResourceFormComponent<Category> {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(protected categoryService: CategoryService, protected injector: Injector) {
+    super(injector, new Category(), categoryService, Category.fromJson);
   }
 
+  // tslint:disable-next-line:typedef
+  protected buildResourceForm() {
+    this.resourceForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  protected creationPageTitle(): string {
+    return 'New Category Registration';
+  }
+
+  protected editionPageTitle(): string {
+    const categoryName = this.resource.name || '';
+    return 'Editing Category: ' + categoryName;
+  }
 }
